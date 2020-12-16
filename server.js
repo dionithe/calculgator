01304664788dc261a68cc62ee43ae826156801ed
Theo <theo@dionisi.ovh>
2020-12-16 10:45:31 +0100

    initialisation du serveur

---
+++ server.js
@@ -0,0 +1,34 @@
+let express = require('express');
+let bodyparser = require('body-parser');
+let app = express();
+let port = 3000;
+
+app.listen(port, () => {
+    console.log('Le serveur est en route');
+    console.log(`Server listening at http://localhost:${port}`);
+});
+
+
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/www'));
+app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js'));
+app.use('/js', express.static(__dirname + '/node_modules/jquery/dist'));
+app.use('/js', express.static(__dirname + '/node_modules/popper.js/dist/umd'));
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
+app.use('/views', express.static(__dirname + '/views'));
+
+
+app.use(bodyparser.urlencoded({extended: false}));
+app.use(bodyparser.json());
+
+
+// Routes
+app.get('/', (req, res, next) => {
+    res.render('index.ejs');
+});
+
+
+
+app.use((req, res, next)=>{
+    res.status(404).render('error.ejs');
+});
