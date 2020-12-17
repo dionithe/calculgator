ba547b1c02fb8451ea9abd7b34dba7e03cfb103c
Théo Dionisi <theo@dionisi.ovh>
2020-12-17 12:34:24 +0100

    ajout du timer

--- www/js/main.js
+++ www/js/main.js
@@ -3,6 +3,10 @@
 // document.write(`${joueur1.nom} a ${joueur1.obtenirScore()} points !`);
 
 
+
+/*
+* Retourne l'objet calcul, contenant le calcul et la reponse
+*/
 function nouveauCalcul() {
 	let a = Math.round(Math.random() * 12);
 	let b = Math.round(Math.random() * 10);
@@ -36,6 +40,50 @@ function nouveauCalcul() {
 	return calcul;
 }
 
-let calcul = nouveauCalcul();
-document.write(`Calcul : ${calcul.question}
-				Réponse : ${calcul.reponse}`);
+
+
+
+function timer() {
+	if(tempsTimer >= 0){
+		$("#timer").text(tempsTimer);
+		tempsTimer --;
+		timerSuivant = setTimeout(timer, 1000);
+	} else {
+		$("#resultat").text("Temps écoulé !");
+	}	
+}
+
+function lancerCalcul(joueur){
+
+	let calcul = nouveauCalcul();
+
+	$("#champCalcul").text(calcul.question);
+	time = timer();
+
+	$("#boutonValider").click(function(){
+		clearTimeout(time);
+		clearTimeout(timerSuivant);
+		$("#boutonValider").unbind();
+
+		console.log(tempsTimer);
+		if ($("#champReponse").val() == calcul.reponse && tempsTimer > 0){
+			joueur.ajouterScore(10);
+			$("#resultat").text("Bon !");
+		} else {
+			$("#resultat").text("Faux !");
+		}
+
+		setTimeout(function(){
+						$("#champReponse").val('');
+						tempsTimer = 15;
+						lancerCalcul(joueur1);
+					}
+					,2000);
+	});
+
+
+}
+
+let tempsTimer = 15;
+let joueur1 = new Joueur("lulu");
+lancerCalcul(joueur1);
